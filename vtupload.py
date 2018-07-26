c962216cdc7c0b1f0ae0f2fb63dec9d8c9c90b9c
AgentE382 <AgentE382@gmail.com>
2018-07-26 18:02:50 -0400

    Mock up simple interface & start implementing

---
+++ vtupload.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from sys import argv
+
+def check_file(path):
+	with open(path, 'rb', buffering = 0) as f:
+		# for b in iter(:
+		# 	pass
+
+if __name__ == '__main__':
+	check_file(argv[0])
