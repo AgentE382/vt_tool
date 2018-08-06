34b9598272cbc412ce7be70658413ffdefaa569f
AgentE382 <AgentE382@gmail.com>
2018-08-06 17:37:37 -0400

    Fix bug where driver didn't use API key

--- vtupload.py
+++ vtupload.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from sys import argv
-from vtapi import Public as vt
+from vtapi import Public
 
 if __name__ == '__main__':
+	vt = Public(api_key ='<change this to yours until I implement the key parameter and per-user saved settings>')
+
 	result = vt.check_file(argv[0])
 	if result != 0:
 		print('INFECTED')
-		
